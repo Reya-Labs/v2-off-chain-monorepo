442ab7ee4547ef061f7799709ae3e19b7a34b27c
Artur Begyan <arturbegyan98@gmail.com>
2023-05-03 17:24:42 +0100

    feat: simple

--- release.config.ts
+++ release.config.ts
@@ -1,27 +1,30 @@
-import { Options as CommitAnalyzerOptions } from '@semantic-release/commit-analyzer';
-import { Options as ReleaseNotesGeneratorOptions } from '@semantic-release/release-notes-generator';
-import { Options as ChangelogOptions } from '@semantic-release/changelog';
+// import { Options as CommitAnalyzerOptions } from '@semantic-release/commit-analyzer';
+// import { Options as ReleaseNotesGeneratorOptions } from '@semantic-release/release-notes-generator';
+// import { Options as ChangelogOptions } from '@semantic-release/changelog';
+// import { Options as NpmOptions } from '@semantic-release/npm';
+// import { Options as GithubOptions } from '@semantic-release/github';
+// import { Options as GitOptions } from '@semantic-release/git';
+//
+// const pkgRoot = process.env.PKG_ROOT || '.';
+//
+// type PluginConfig =
+//   | string
+//   | [string, CommitAnalyzerOptions]
+//   | [string, ReleaseNotesGeneratorOptions]
+//   | [string, ChangelogOptions]
+//   | [string, NpmOptions]
+//   | [string, GithubOptions]
+//   | [string, GitOptions];
+
 import { Options as NpmOptions } from '@semantic-release/npm';
-import { Options as GithubOptions } from '@semantic-release/github';
-import { Options as GitOptions } from '@semantic-release/git';
 
 const pkgRoot = process.env.PKG_ROOT || '.';
 
-type PluginConfig =
-  | string
-  | [string, CommitAnalyzerOptions]
-  | [string, ReleaseNotesGeneratorOptions]
-  | [string, ChangelogOptions]
-  | [string, NpmOptions]
-  | [string, GithubOptions]
-  | [string, GitOptions];
+type PluginConfig = string | [string, NpmOptions];
 
 const config: { [key: string]: any } = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
@@ -34,16 +37,37 @@ const config: { [key: string]: any } = {
         pkgRoot: pkgRoot as NpmOptions,
       },
     ],
-    '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message:
-          'chore(release): set ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
   ] as PluginConfig[],
 };
+
+// const config: { [key: string]: any } = {
+//   branches: ['main'],
+//   plugins: [
+//     '@semantic-release/commit-analyzer',
+//     '@semantic-release/release-notes-generator',
+//     '@semantic-release/changelog',
+//     [
+//       '@semantic-release/npm',
+//       {
+//         npmPublish: true,
+//         tarballDir: 'dist',
+//         publishConfig: {
+//           registry: 'https://npm.pkg.github.com',
+//         },
+//         // For a monorepo, update the 'pkgRoot' to the respective package directory, e.g. 'packages/api'.
+//         pkgRoot: pkgRoot as NpmOptions,
+//       },
+//     ],
+//     '@semantic-release/github',
+//     [
+//       '@semantic-release/git',
+//       {
+//         assets: ['CHANGELOG.md', 'package.json'],
+//         message:
+//           'chore(release): set ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+//       },
+//     ],
+//   ] as PluginConfig[],
+// };
 
 export default config;
