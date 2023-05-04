4dc2e7b259f40b0d23e9a26a3325f5e9a86ca9ed
Artur Begyan <arturbegyan98@gmail.com>
2023-05-04 21:13:05 +0100

    feat: trim down semantic release config

--- release.config.ts
+++ release.config.ts
@@ -1,18 +1,14 @@
 import { Options as CommitAnalyzerOptions } from '@semantic-release/commit-analyzer';
 import { Options as ReleaseNotesGeneratorOptions } from '@semantic-release/release-notes-generator';
 import { Options as ChangelogOptions } from '@semantic-release/changelog';
-import { Options as NpmOptions } from '@semantic-release/npm';
 import { Options as GithubOptions } from '@semantic-release/github';
 import { Options as GitOptions } from '@semantic-release/git';
-
-const pkgRoot = process.env.PKG_ROOT || '.';
 
 type PluginConfig =
   | string
   | [string, CommitAnalyzerOptions]
   | [string, ReleaseNotesGeneratorOptions]
   | [string, ChangelogOptions]
-  | [string, NpmOptions]
   | [string, GithubOptions]
   | [string, GitOptions];
 
@@ -22,17 +18,6 @@ const config: { [key: string]: any } = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    [
-      '@semantic-release/npm',
-      {
-        npmPublish: true,
-        tarballDir: 'dist',
-        publishConfig: {
-          registry: 'https://registry.npmjs.org/',
-        },
-        pkgRoot: pkgRoot as NpmOptions,
-      },
-    ],
     '@semantic-release/github',
     [
       '@semantic-release/git',
