e3716763340101d7cba4c41bf8bc2af67b2b0ff5
Artur Begyan <arturbegyan98@gmail.com>
2023-05-08 13:55:41 +0100

    feat: skeleton for input and initiate taker order pipeline

--- packages/transformer/src/pipelines/initiate_taker_order_pipeline.py
+++ packages/transformer/src/pipelines/initiate_taker_order_pipeline.py
@@ -1,15 +1,16 @@
-# Import necessary modules
+from typing import List
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 # TODO: check if the import is done correctly
 from packages.transformer.src.io import input, output
 from packages.transformer.src.transformations import simple_transformation
 
-# Define the pipeline
-def run_initiate_taker_order_pipeline(pipeline_options: PipelineOptions):
+# todo: needs restructuring
+def run_initiate_taker_order_pipeline(pipeline_options: PipelineOptions, topics: List[str]):
     with beam.Pipeline(options=pipeline_options) as p:
         # Read data from Pub/Sub using input module
-        input_data = p | 'Read from Pub/Sub' >> input.read_from_pubsub()
+        input_data = p | 'Read from Pub/Sub' >> input.read_from_pubsub(topics)
+
 
         # Apply transformations
         transformed_data = input_data | 'Apply transformations' >> beam.ParDo(simple_transformation.SimpleTransformation())
