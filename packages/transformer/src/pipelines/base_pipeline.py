835d127353c4c8eef91bce24aad47d32c6304849
Artur Begyan <arturbegyan98@gmail.com>
2023-05-10 22:46:28 +0100

    feat: simple batch pipeline

--- packages/transformer/src/pipelines/base_pipeline.py
+++ packages/transformer/src/pipelines/base_pipeline.py
@@ -1,12 +1,19 @@
+from __future__ import absolute_import
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.transforms import PTransform
 
-def run_pipeline(pipeline_options: PipelineOptions, readTransform: PTransform, writeTransform: PTransform, transformation: PTransform=None):
-    with beam.Pipeline(options=pipeline_options) as pipeline:
+def run_base_pipeline(pipeline_options: PipelineOptions, readTransform: PTransform, writeTransform: PTransform, transformation: PTransform=None):
+
+    pipeline = beam.Pipeline(options=pipeline_options)
+
+    with pipeline as pipeline:
+
         data = pipeline | 'Reading' >> readTransform
 
         if transformation:
             data = data | 'Apply Transformation' >> transformation
 
-        data = data | 'Writing' >> writeTransform
+        (data | 'Writing' >> writeTransform)
+
+        pipeline.run().wait_until_finish()
--- packages/transformer/src/pipelines/no_transformation.py
+++ packages/transformer/src/pipelines/no_transformation.py
@@ -1,6 +1,6 @@
-from base_pipeline import run_pipeline
+from base_pipeline import run_base_pipeline
 from apache_beam.transforms import PTransform
 from apache_beam.options.pipeline_options import PipelineOptions
 
 def run_no_transformation_pipeline(pipeline_options: PipelineOptions, readTransform: PTransform, writeTransform: PTransform):
-    run_pipeline(pipeline_options, readTransform, writeTransform)
+    run_base_pipeline(pipeline_options, readTransform, writeTransform)
