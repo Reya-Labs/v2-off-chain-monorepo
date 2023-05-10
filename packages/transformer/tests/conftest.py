50de8a20d60d1db55dd10855d88c1288aaef9bce
Artur Begyan <arturbegyan98@gmail.com>
2023-05-10 21:23:16 +0100

    feat: rename

--- packages/transformer/tests/conftest.py
+++ packages/transformer/tests/conftest.py
@@ -2,6 +2,6 @@ import pytest
 import json
 
 @pytest.fixture
-def test_generic_input_data():
+def no_transformation_pipeline_mock_data():
     with open('mock-data/no_transformation_pipeline.json') as f:
         return json.load(f)
