1d346fbacab0a86c39b5457f8b94c1e08e610a9d
Artur Begyan <arturbegyan98@gmail.com>
2023-05-10 19:15:12 +0100

    feat: simplify

--- packages/transformer/src/pipelines/pnl_pipeline.py
+++ packages/transformer/src/pipelines/pnl_pipeline.py
@@ -1,10 +1,10 @@
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from packages.transformer.src.io import input, output # todo: is this best import pattern?
+from apache_beam.transforms import PTransform
 
-
-def run_pipeline(pipeline_options: PipelineOptions, transformation: ):
+def run_pipeline(pipeline_options: PipelineOptions, transformation: PTransform):
     with beam.Pipeline(options=pipeline_options) as p:
-        input_data = p | 'Read from Pub/Sub' >> input.read()
-        transformed_data = input_data | 'Apply PnL Transformation' >> beam.ParDo(pnl_transformation.PnLTransformation())
-        output_data = transformed_data | 'Write to Bigtable' >> output.write_to_bigtable()
+        input_data = p | 'Reading' >> input.read()
+        transformed_data = input_data | 'Apply Transformation' >> transformation
+        output_data = transformed_data | 'Writing' >> output.write()
