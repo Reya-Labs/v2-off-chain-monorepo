eb16368411d7b322183dded264d4d5fb6ddfedd7
Artur Begyan <arturbegyan98@gmail.com>
2023-05-10 19:23:55 +0100

    feat: read and write transform typings

--- packages/transformer/src/pipelines/pnl_pipeline.py
+++ packages/transformer/src/pipelines/pnl_pipeline.py
@@ -1,9 +1,10 @@
 import apache_beam as beam
+from apache_beam.io.iobase import Read, Write
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.transforms import PTransform
 
-def run_pipeline(pipeline_options: PipelineOptions, transformation: PTransform, readTransform: PTransform, writeTransform: PTransform):
-    with beam.Pipeline(options=pipeline_options) as p:
-        input_data = p | 'Reading' >> readTransform
+def run_pipeline(pipeline_options: PipelineOptions, transformation: PTransform, readTransform: Read, writeTransform: Write):
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+        input_data = pipeline | 'Reading' >> readTransform
         transformed_data = input_data | 'Apply Transformation' >> transformation
         output_data = transformed_data | 'Writing' >> writeTransform
