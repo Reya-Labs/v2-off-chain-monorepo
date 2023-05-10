5dd06f1ada094ad13a1de7d813d0156b98d767f0
Artur Begyan <arturbegyan98@gmail.com>
2023-05-10 19:10:06 +0100

    feat: pnl pipeline setup

--- packages/transformer/src/pipelines/pnl_pipeline.py
+++ packages/transformer/src/pipelines/pnl_pipeline.py
@@ -2,6 +2,7 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from packages.transformer.src.io import input, output # todo: is this best import pattern?
 
+
 def run_pipeline(pipeline_options: PipelineOptions, transformation: ):
     with beam.Pipeline(options=pipeline_options) as p:
         input_data = p | 'Read from Pub/Sub' >> input.read()
