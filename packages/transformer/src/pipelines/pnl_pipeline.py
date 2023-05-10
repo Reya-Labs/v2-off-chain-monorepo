9169b8100d713191a1357b295a55bc9ffd3fc063
Artur Begyan <arturbegyan98@gmail.com>
2023-05-10 19:18:02 +0100

    feat: generic pipeline

--- packages/transformer/src/pipelines/pnl_pipeline.py
+++ packages/transformer/src/pipelines/pnl_pipeline.py
@@ -2,8 +2,8 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.transforms import PTransform
 
-def run_pipeline(pipeline_options: PipelineOptions, transformation: PTransform, input: PTransform, output: PTransform):
+def run_pipeline(pipeline_options: PipelineOptions, transformation: PTransform, readTransform: PTransform, writeTransform: PTransform):
     with beam.Pipeline(options=pipeline_options) as p:
-        input_data = p | 'Reading' >> input.read()
+        input_data = p | 'Reading' >> readTransform
         transformed_data = input_data | 'Apply Transformation' >> transformation
-        output_data = transformed_data | 'Writing' >> output.write()
+        output_data = transformed_data | 'Writing' >> writeTransform
