c46df71eae1442edd599621bec649f41479831ba
tudor47 <tudorcostin7@gmail.com>
2023-05-13 18:43:32 +0300

    feat: big table prep work

---
+++ packages/indexer/src/services/big-table/taker-orders-table/pullAllTakerOrderRows.ts
@@ -0,0 +1,18 @@
+import { TakerOrderEvent } from '../../../event-parsers/types';
+import { getTable, mapBigtableRow } from './utils';
+
+export const pullAllTakerOrderRows = async (): Promise<TakerOrderEvent[]> => {
+  const table = getTable();
+
+  const filter = [
+    {
+      column: {
+        cellLimit: 1, // Only retrieve the most recent version of the cell.
+      },
+    },
+  ];
+
+  const [allRows] = await table.getRows({ filter });
+
+  return allRows.map(mapBigtableRow);
+};
--- packages/indexer/tests/event-parsers/takerOrder.test.ts
+++ packages/indexer/tests/event-parsers/takerOrder.test.ts
@@ -1,7 +1,7 @@
 import { BigNumber, Event } from 'ethers';
 import { parseTakerOrder } from '../../src/event-parsers/dated-irs-vamm/takerOrder';
-import { compareEvents } from './compareEvents';
-import { TakerOrderEvent } from '../../src/event-parsers/types';
+import { compareEvents } from '../utils/compareEvents';
+import { takerOrderEvent } from '../utils/mocks';
 
 describe('taker order parser', () => {
   test('usual event', () => {
@@ -41,38 +41,8 @@ describe('taker order parser', () => {
       },
     } as unknown as Event;
 
-    const takerOrderEvent = parseTakerOrder(chainId, event);
+    const outputTakerOrderEvent = parseTakerOrder(chainId, event);
 
-    console.log('taker order event:', takerOrderEvent);
-
-    const expectedTakerOrderEvent: TakerOrderEvent = {
-      id: '1_taker-order_Block-Hash_0x2ef67d6f04295106894d762e66c6fd39ba36c02d43dac503df0bc7272803f40A_124',
-      type: 'taker-order',
-
-      chainId: 1,
-      source: '0xe9a6569995f3d8ec971f1d314e0e832c38a735cc',
-
-      blockTimestamp: 1683092975,
-      blockNumber: 17178234,
-      blockHash: 'Block-Hash',
-
-      transactionIndex: 21,
-      transactionHash:
-        '0x2ef67d6f04295106894d762e66c6fd39ba36c02d43dac503df0bc7272803f40A',
-      logIndex: 124,
-
-      accountId: '1000000000',
-
-      marketId: '1111111111',
-      maturityTimestamp: 1685534400,
-      quoteToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-
-      executedBaseAmount: 100,
-      executedQuoteAmount: -550,
-
-      annualizedBaseAmount: 7.5,
-    };
-
-    expect(compareEvents(takerOrderEvent, expectedTakerOrderEvent)).toBe(null);
+    expect(compareEvents(outputTakerOrderEvent, takerOrderEvent)).toBe(null);
   });
 });
