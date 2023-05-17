e7626366ea96f87e719990b6fd34a9fd2c153010
Artur Begyan <arturbegyan98@gmail.com>
2023-05-17 23:06:16 +0100

    feat: stateful taker position transform

--- packages/transformer/src/transformations/dated-irs-taker-positions/stateful_taker_position_transform.py
+++ packages/transformer/src/transformations/dated-irs-taker-positions/stateful_taker_position_transform.py
@@ -16,13 +16,31 @@ class StatefulTakerPositionTransformDoFn(beam.DoFn):
         rateOracleIndex, cashflow_li_factor, cashflow_time_factor, cashflow_free_term
 
         to keep things simple start with position_id, timestamp & realized_pnl_from_fees_paid
+
+        need to get this from the initiate taker order event:
+        timestamp, accountId, maturityId, maturityTimestamp, executedBaseAmount, executedQuoteAmount, rateOracleIndex, feesPaid
+
+        should be parallelised for each positionid
     '''
 
-
     TAKER_POSITION_STATE = BagStateSpec('taker_position', TupleCoder((StrUtf8Coder, TimestampCoder, BigIntegerCoder)))
-    def process(self, event, cached_taker_position_state=beam.DoFn.StateParam(TAKER_POSITION_STATE)):
 
-        pass
+    def process(self, initiateTakerOrderEvent, cached_taker_position_state=beam.DoFn.StateParam(TAKER_POSITION_STATE)):
 
+        fees_paid_to_initiate_taker_order = initiateTakerOrderEvent['feesPaid']
+        current_taker_order_event_timestamp = initiateTakerOrderEvent['timestamp']
+        position_id = initiateTakerOrderEvent['positionId']
 
+        cached_taker_position_state_list = [x for x in cached_taker_position_state.read()]
 
+        if len(cached_taker_position_state_list)>0:
+            position_id = cached_taker_position_state_list[0]
+            last_realized_pnl_from_fees_paid = cached_taker_position_state_list[2]
+            cached_taker_position_state.clear()
+            current_realized_pnl_from_fees_paid = last_realized_pnl_from_fees_paid + fees_paid_to_initiate_taker_order
+            cached_taker_position_state.add(position_id, current_taker_order_event_timestamp, current_realized_pnl_from_fees_paid)
+        else:
+            current_realized_pnl_from_fees_paid = fees_paid_to_initiate_taker_order
+
+        cached_taker_position_state.add(position_id, current_taker_order_event_timestamp, fees_paid_to_initiate_taker_order)
+        yield position_id, current_taker_order_event_timestamp, current_realized_pnl_from_fees_paid
