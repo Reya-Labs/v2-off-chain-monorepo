1caf96ef3d4848f3b529dadf6d0e3ee380592aef
Artur Begyan <arturbegyan98@gmail.com>
2023-05-17 16:14:20 +0100

    feat: tests that the test stream supports emitting to multiple pcollections

--- packages/transformer/tests/pipelines/test_stream_pipeline.py
+++ packages/transformer/tests/pipelines/test_stream_pipeline.py
@@ -11,6 +11,7 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import StandardOptions
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
+from apache_beam.utils.timestamp import Timestamp
 
 
 # todo: have some sort of module for mocks and test supporting utils like this one
@@ -20,8 +21,6 @@ class RecordFn(beam.DoFn):
             element=beam.DoFn.ElementParam,
             timestamp=beam.DoFn.TimestampParam):
         yield (element, timestamp)
-
-
 
 def test_basic_execution_test_stream():
     test_stream = (TestStream()
@@ -73,3 +72,49 @@ def test_basic_execution_test_stream():
                 ('late', timestamp.Timestamp(12)),
                 ('last', timestamp.Timestamp(310)),
             ]))
+
+
+def test_multiple_outputs():
+    """Tests that the TestStream supports emitting to multiple PCollections."""
+
+    letters_elements = [
+        TimestampedValue('a', 6),
+        TimestampedValue('b', 7),
+        TimestampedValue('c', 8),
+    ]
+
+    numbers_elements = [
+        TimestampedValue('1', 11),
+        TimestampedValue('2', 12),
+        TimestampedValue('3', 13),
+    ]
+
+    test_stream = (TestStream()
+        .advance_watermark_to(5, tag='letters')
+        .add_elements(letters_elements, tag='letters')
+        .advance_watermark_to(10, tag='numbers')
+        .add_elements(numbers_elements, tag='numbers'))
+
+    options = StandardOptions(streaming=True)
+    test_pipeline = TestPipeline(options=options)
+
+    inputCollection = test_pipeline | test_stream
+
+    letters = inputCollection['letters'] | 'record letters' >> beam.ParDo(RecordFn())
+    numbers = inputCollection['numbers'] | 'record numbers' >> beam.ParDo(RecordFn())
+
+    assert_that(
+        letters,
+        equal_to([('a', Timestamp(6)), ('b', Timestamp(7)),
+                  ('c', Timestamp(8))]),
+        label='assert letters')
+
+    assert_that(
+        numbers,
+        equal_to([('1', Timestamp(11)), ('2', Timestamp(12)),
+                  ('3', Timestamp(13))]),
+        label='assert numbers')
+
+    test_pipeline.run()
+
+
