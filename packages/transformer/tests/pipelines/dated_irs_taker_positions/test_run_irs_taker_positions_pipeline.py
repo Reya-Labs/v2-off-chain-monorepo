92154d9a6efc1f70d723fdcbf7372275d15d8555
Artur Begyan <arturbegyan98@gmail.com>
2023-05-18 16:48:52 +0100

    feat: add an assert to the dated irs takers pipeline test

--- packages/transformer/tests/pipelines/dated_irs_taker_positions/test_run_irs_taker_positions_pipeline.py
+++ packages/transformer/tests/pipelines/dated_irs_taker_positions/test_run_irs_taker_positions_pipeline.py
@@ -1,9 +1,11 @@
 from packages.transformer.src.pipelines.dated_irs_taker_positions.run import run as run_irs_taker_positions_pipeline
 from apache_beam.testing.test_stream import TestStream
-from apache_beam.transforms.window import TimestampedValue
+from apache_beam.transforms.window import TimestampedValue, GlobalWindow
 from apache_beam.options.pipeline_options import StandardOptions
+from apache_beam.testing.util import assert_that
+from apache_beam.testing.util import equal_to_per_window
 
-def test_run():
+def test_run_irs_taker_positions_pipeline():
 
     # todo: introduce python types for the messages received from pub-sub in io/pubsub
 
@@ -19,9 +21,16 @@ def test_run():
 
     pipeline_options = StandardOptions(streaming=True)
 
-    run_irs_taker_positions_pipeline(
+    updated_dated_irs_taker_positions_global_windows = run_irs_taker_positions_pipeline(
         pipeline_options=pipeline_options,
         initiate_taker_order_events_stream=test_initiate_taker_order_events_stream
     )
 
-    # do assertions here
+    expected_updated_dated_irs_taker_positions_global_windows = {
+        GlobalWindow(): [10, 20, 30, 40],
+    }
+
+    assert_that(
+        updated_dated_irs_taker_positions_global_windows,
+        equal_to_per_window(expected_updated_dated_irs_taker_positions_global_windows),
+        label='dated irs taker positions assert')
