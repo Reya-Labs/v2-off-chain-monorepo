295cc16d3b29e38557ffea529f1d7394fb22ac34
Artur Begyan <arturbegyan98@gmail.com>
2023-05-18 23:07:54 +0100

    feat: executed base and quote amounts

---
+++ packages/transformer/src/helpers/executed_notional_from_base.py
@@ -0,0 +1,10 @@
+
+
+def executed_notional_from_base(base_amount: float, rate_index: float):
+    '''
+
+    :param base_amount:
+    :param rate_index:
+    :return:
+    '''
+    return base_amount * rate_index
--- packages/transformer/src/helpers/notional_from_base.py
+++
@@ -1,4 +0,0 @@
-
-
-def notional_from_base(base_amount: float, rate_index: float):
-    return base_amount * rate_index
