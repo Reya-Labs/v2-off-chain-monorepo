830e339be835c9e72d9b4c715ff7b805fac88fa9
Artur Begyan <arturbegyan98@gmail.com>
2023-05-18 16:21:46 +0100

    feat: stateful processing of dated irs taker positions

--- packages/transformer/src/pipelines/dated_irs_taker_positions/run.py
+++ packages/transformer/src/pipelines/dated_irs_taker_positions/run.py
@@ -5,8 +5,6 @@ import apache_beam as beam
 def run(pipeline_options, initiate_taker_order_events_stream):
     dated_irs_taker_position_pipeline = beam.Pipeline()
     initiate_taker_order_events = dated_irs_taker_position_pipeline | initiate_taker_order_events_stream
-    # initiate_taker_order_events
-    # stopped here
-
+    updated_dated_irs_taker_positions = initiate_taker_order_events | "BagStatefulProcessTakerPositions" >> beam.ParDo(StatefulTakerPositionTransformDoFn())
 
 
