fb89960f1c957eeced895060bf187b347c5a9eea
Artur Begyan <arturbegyan98@gmail.com>
2023-05-19 19:58:10 +0100

    feat: dated irs taker position run

--- packages/transformer/src/main.py
+++ packages/transformer/src/main.py
@@ -1,5 +1,6 @@
 import argparse
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam import Pipeline
 import logging
 from packages.transformer.src.pipelines.dated_irs_taker_positions.generate_pipeline_and_output import generate_dated_irs_taker_positions_pipeline_and_output
 
@@ -19,16 +20,31 @@ def run():
         help="Name of the pipeline"
     )
 
-    args = parser.parse_args()
+    args, beam_args = parser.parse_known_args()
 
+    # todo: specify: temp_location='gs://my-bucket/temp',
     pipeline_options = PipelineOptions(
+        runner='DataflowRunner',
         streaming=True,
         save_main_session=True,
         job_name=args.pipeline,
         num_workers=10,
-        max_num_workers=10
+        max_num_workers=10,
+        region='eu-west2'
     )
 
+    pipeline_name = beam_args['pipeline']
+    input_stream = beam_args['input']
+    output_database = beam_args['output']
+    pipeline = Pipeline(options=pipeline_options)
+
+    if pipeline_name == "dated_irs_taker_positions":
+        # todo: incorporate output database
+        dated_irs_taker_position_pipeline, updated_dated_irs_taker_positions_global_windows = generate_dated_irs_taker_positions_pipeline_and_output(
+            dated_irs_taker_position_pipeline=pipeline,
+            initiate_taker_order_events_stream=input_stream,
+        )
+        dated_irs_taker_position_pipeline.run()
 
 
 if __name__ == "__main__":
