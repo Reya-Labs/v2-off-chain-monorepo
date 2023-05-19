a2480a8d2b60beb0505f316a616f02333b880bd5
Artur Begyan <arturbegyan98@gmail.com>
2023-05-19 20:41:21 +0100

    feat: get write to bigquery transform setuo

--- packages/transformer/src/io/bigquery/write_to_bigquery.py
+++ packages/transformer/src/io/bigquery/write_to_bigquery.py
@@ -1,8 +1,19 @@
+from apache_beam.io.gcp.internal.clients.bigquery import TableReference, TableSchema
+from apache_beam.io import BigQueryDisposition
+from apache_beam.io import WriteToBigQuery
 
 
 
 
+def get_write_to_bigquery_transform(table_reference: TableReference, table_schema: TableSchema,
+                                    write_disposition: BigQueryDisposition, create_disposition: BigQueryDisposition):
+
+    return WriteToBigQuery(
+       table=table_reference,
+       schema=table_schema,
+       write_disposition=write_disposition,
+       create_disposition=create_disposition
+    )
 
 
-def get_write_to_bigquery_transform():
-    pass
+
