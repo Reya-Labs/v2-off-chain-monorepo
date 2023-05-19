ac82f42cef7011f52d29d64602dad77830d71207
Artur Begyan <arturbegyan98@gmail.com>
2023-05-19 21:05:35 +0100

    feat: wrap up dated irs taker positions schema

--- packages/transformer/src/io/bigquery/schemas/dated_irs_taker_positions_schema.py
+++ packages/transformer/src/io/bigquery/schemas/dated_irs_taker_positions_schema.py
@@ -1,11 +1,20 @@
 from apache_beam.io.gcp.internal.clients.bigquery import TableSchema, TableFieldSchema
 
-def get_dated_irs_taker_positions_schema():
+def get_dated_irs_taker_positions_schema() -> TableSchema:
+
+    """
+    timestamp, chain_id, instrument_id, market_id, maturity_timestamp, account_id,
+    realized_pnl_from_swaps, realized_pnl_from_fees_paid, net_notional_locked,
+    net_fixed_rate_locked, base_balance, quote_balance, rateOracleIndex
+    """
+
     table_schema = TableSchema()
 
-    # chain_id, instrument_id, market_id, maturity_timestamp, account_id,
-    # realized_pnl_from_swaps, realized_pnl_from_fees_paid, net_notional_locked,
-    # net_fixed_rate_locked, timestamp, rateOracleIndex
+    timestamp_schema = TableFieldSchema()
+    timestamp_schema.name = "timestamp"
+    timestamp_schema.type = "timestamp"
+    timestamp_schema.mode = "required"
+    table_schema.fields.append(timestamp_schema)
 
     chain_id_schema = TableFieldSchema()
     chain_id_schema.name = "chain_id"
@@ -43,7 +52,43 @@ def get_dated_irs_taker_positions_schema():
     realized_pnl_from_swaps_schema.mode = "required"
     table_schema.fields.append(realized_pnl_from_swaps_schema)
 
+    realized_pnl_from_fees_paid_schema = TableFieldSchema()
+    realized_pnl_from_fees_paid_schema.name = "realized_pnl_from_fees_paid"
+    realized_pnl_from_fees_paid_schema.type = "numeric"
+    realized_pnl_from_fees_paid_schema.mode = "required"
+    table_schema.fields.append(realized_pnl_from_fees_paid_schema)
 
+    net_notional_locked_schema = TableFieldSchema()
+    net_notional_locked_schema.name = "net_notional_locked"
+    net_notional_locked_schema.type = "numeric"
+    net_notional_locked_schema.mode = "required"
+    table_schema.fields.append(net_notional_locked_schema)
+
+    net_fixed_rate_locked_schema = TableFieldSchema()
+    net_fixed_rate_locked_schema.name = "net_fixed_rate_locked"
+    net_fixed_rate_locked_schema.type = "numeric"
+    net_fixed_rate_locked_schema.mode = "required"
+    table_schema.fields.append(net_fixed_rate_locked_schema)
+
+    base_balance_schema = TableFieldSchema()
+    base_balance_schema.name = "base_balance"
+    base_balance_schema.type = "numeric"
+    base_balance_schema.mode = "required"
+    table_schema.fields.append(base_balance_schema)
+
+    quote_balance_schema = TableFieldSchema()
+    quote_balance_schema.name = "quote_balance"
+    quote_balance_schema.type = "numeric"
+    quote_balance_schema.mode = "required"
+    table_schema.fields.append(quote_balance_schema)
+
+    rate_oracle_index_schema = TableFieldSchema()
+    rate_oracle_index_schema.name = "rate_oracle_index"
+    rate_oracle_index_schema.type = "numeric"
+    rate_oracle_index_schema.mode = "required"
+    table_schema.fields.append(rate_oracle_index_schema)
+
+    return table_schema
 
 
 
