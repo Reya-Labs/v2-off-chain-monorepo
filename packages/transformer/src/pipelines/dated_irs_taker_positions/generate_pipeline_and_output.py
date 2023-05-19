64de0788230343c33a25f9d2d7d54503590d3c4e
Artur Begyan <arturbegyan98@gmail.com>
2023-05-19 20:05:15 +0100

    feat: enrich pipeline generation to accept input and output

--- packages/transformer/src/pipelines/dated_irs_taker_positions/generate_pipeline_and_output.py
+++ packages/transformer/src/pipelines/dated_irs_taker_positions/generate_pipeline_and_output.py
@@ -1,6 +1,9 @@
 from packages.transformer.src.transformations.dated_irs_taker_positions.stateful_taker_position_transform import StatefulTakerPositionTransformDoFn
 import apache_beam as beam
 from apache_beam.transforms import trigger
+from apache_beam import PTransform, Pipeline
+from apache_beam.testing.test_pipeline import TestPipeline
+from typing import Union
 
 # todo: move to constants
 GLOBAL_WINDOW_AFTER_COUNT = 1
@@ -9,7 +12,11 @@ class SetKeyToPositionId(beam.DoFn):
     def process(self, element: dict):
         yield element['position_id'], element
 
-def generate_dated_irs_taker_positions_pipeline_and_output(dated_irs_taker_position_pipeline, initiate_taker_order_events_stream):
+def generate_dated_irs_taker_positions_pipeline_and_output(
+        dated_irs_taker_position_pipeline: Union[Pipeline, TestPipeline],
+        initiate_taker_order_events_stream: PTransform,
+        destination_transform: PTransform
+        ):
     '''
     consider using this windowing approach (need to confirm what AfterProcessingTime means)
     beam.WindowInto(GlobalWindows(), trigger=trigger.AfterAll(trigger.AfterCount(1), trigger.AfterProcessingTime(1)))
@@ -24,5 +31,7 @@ def generate_dated_irs_taker_positions_pipeline_and_output(dated_irs_taker_posit
         accumulation_mode=beam.trigger.AccumulationMode.ACCUMULATING,
         allowed_lateness=0
     )
+    if destination_transform:
+        updated_dated_irs_taker_positions_global_windows = updated_dated_irs_taker_positions_global_windows | destination_transform
 
     return dated_irs_taker_position_pipeline, updated_dated_irs_taker_positions_global_windows
