df7a9e29374e98b89d50b0b71200694502c0c581
Artur Begyan <arturbegyan98@gmail.com>
2023-05-23 17:42:59 +0100

    feat: layer in mvp abi for periphery contract

--- packages/v1-sdk/src/common/contract-generators/getPeripheryContract.ts
+++ packages/v1-sdk/src/common/contract-generators/getPeripheryContract.ts
@@ -4,9 +4,39 @@ export const getPeripheryContract =  (
   peripheryAddress: string,
   provider: ethers.providers.Provider
 ): ethers.Contract => {
-  // todo: only bring the relevant abi
-  const abi = [
-    ``
+
+  // reference periphery interface example
+  /*
+     https://github.com/Voltz-Protocol/voltz-core/blob/main/contracts/interfaces/IPeriphery.sol#L89
+     struct SwapPeripheryParams {
+        IMarginEngine marginEngine;
+        bool isFT;
+        uint256 notional;
+        uint160 sqrtPriceLimitX96;
+        int24 tickLower;
+        int24 tickUpper;
+        int256 marginDelta;
+    }
+
+    function swap(SwapPeripheryParams memory params)
+        external
+        payable
+        returns (
+            int256 _fixedTokenDelta,
+            int256 _variableTokenDelta,
+            uint256 _cumulativeFeeIncurred,
+            int256 _fixedTokenDeltaUnbalanced,
+            int256 _marginRequirement,
+            int24 _tickAfter,
+            int256 marginDelta
+        );
+   */
+
+  // todo: needs to be tested separately
+  const abi: string[] = [
+    `
+    function swap(address, bool, uint256, uint160, int24, int24, int256) external payable returns (int256,int256,uint256,int256,int256,int24,int256)
+    `
   ];
 
   const contract: ethers.Contract = new ethers.Contract(peripheryAddress, abi, provider);
