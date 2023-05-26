84dfc38dc89dc1b3647e4708c155847a1eac5b63
Artur Begyan <arturbegyan98@gmail.com>
2023-05-26 20:14:57 +0100

    feat: swap test setup

---
+++ packages/v1-sdk/tests/actions/swap/swap.test.ts
@@ -0,0 +1,52 @@
+// Imports required for test setup
+import {SwapArgs} from "../../../src/actions/types/actionArgTypes";
+import { BigNumber, providers, Signer, utils } from "ethers";
+import { ethers } from 'ethers';
+import { swap} from "../../../src/actions/swap/swap";
+
+// Mocks for the dependencies
+// todo: the resolved value of the swap function mock of the periphery should change to a more realistic value
+jest.mock('../../common/contract-generators/getPeripheryContract', () => ({
+  getPeripheryContract: jest.fn(() => ({
+    connect: jest.fn().mockReturnThis(),
+    swap: jest.fn().mockResolvedValue('Swap successful'),
+  })),
+}));
+
+// todo: we can just mock the getDefaultProvider() function for the mockSwapArgs using jest
+// todo: same goes for the signer
+
+describe('Swap', () => {
+
+  it("should handle swap successfully", async () => {
+
+    const mockSwapArgs: SwapArgs = {
+      isFT:false,
+      notional:100,
+      margin:100,
+      fixedRateLimit:0.1,
+      fixedLow:0.1,
+      fixedHigh:0.2,
+      underlyingTokenAddress:"0xChadToken",
+      underlyingTokenDecimals:18,
+      tickSpacing:60,
+      chainId:1,
+      peripheryAddress:"0xChadPeriphery",
+      marginEngineAddress:"0xChadMarginEngine",
+      provider:ethers.getDefaultProvider(),
+      signer:None,
+      isEth:false
+    }
+
+    // Execute the swap function
+    const swapResult = await swap();
+
+    // Assert that swap has been successful
+    expect(swapResult).toBe('Swap successful');
+
+    // Verify that the mocks have been called as expected
+    expect(SwapModule.handleSwapErrors).toHaveBeenCalledTimes(1);
+    // Add similar expect statements for other mocked functions
+  })
+
+});
