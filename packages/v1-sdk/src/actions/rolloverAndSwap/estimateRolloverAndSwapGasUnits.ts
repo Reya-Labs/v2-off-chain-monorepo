0780b4bbd3d82564e35bb5c5936b89e6e5c8fcef
Artur Begyan <arturbegyan98@gmail.com>
2023-05-28 19:26:03 +0100

    feat: estimate rollover and swap gas units

---
+++ packages/v1-sdk/src/actions/rolloverAndSwap/estimateRolloverAndSwapGasUnits.ts
@@ -0,0 +1,21 @@
+import { ethers, BigNumber } from 'ethers';
+import { RolloverAndSwapPeripheryParams } from '../types/actionArgTypes';
+
+export const estimateRolloverAndSwapGasUnits = async (
+  peripheryContract: ethers.Contract,
+  rolloverAndSwapPeripheryParams: RolloverAndSwapPeripheryParams,
+  rolloverAndSwapPeripheryTempOverrides: { value?: BigNumber; gasLimit?: BigNumber },
+): Promise<BigNumber> => {
+  const estimatedGas: BigNumber = await peripheryContract.estimateGas
+    .rolloverWithSwap(
+      rolloverAndSwapPeripheryParams,
+      rolloverAndSwapPeripheryTempOverrides,
+    )
+    .catch((error) => {
+      throw new Error(
+        'Error estimating rollover and swap gas units',
+      );
+    });
+
+  return estimatedGas;
+};
