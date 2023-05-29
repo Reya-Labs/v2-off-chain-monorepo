6952b41c75917e60d96ef3f557a615c3798921a9
Artur Begyan <arturbegyan98@gmail.com>
2023-05-29 23:03:17 +0100

    feat: rollover and lp end to end

--- packages/v1-sdk/src/actions/rolloverAndLp/rolloverAndLp.ts
+++ packages/v1-sdk/src/actions/rolloverAndLp/rolloverAndLp.ts
@@ -9,7 +9,87 @@ import { estimateRolloverAndLpGasUnits } from "./estimateRolloverAndSwapGasUnits
 
 export const rolloverAndLp = async (
   {
-  }
+    addLiquidity,
+    fixedLow,
+    fixedHigh,
+    notional,
+    margin,
+    underlyingTokenAddress,
+    underlyingTokenDecimals,
+    tickSpacing,
+    chainId,
+    peripheryAddress,
+    marginEngineAddress,
+    provider,
+    signer,
+    isEth,
+    maturedMarginEngineAddress,
+    maturedPositionOwnerAddress,
+    maturedPositionSettlementBalance,
+    maturedPositionTickLower,
+    maturedPositionTickUpper
+  }: RolloverAndLpArgs
 ): Promise<ContractReceipt> => {
+  handleLpErrors(
+    {
+      notional,
+      fixedLow,
+      fixedHigh,
+    }
+  );
 
+  const peripheryContract: ethers.Contract = getPeripheryContract(
+    peripheryAddress,
+    provider,
+  );
+
+  peripheryContract.connect(signer);
+
+  const rolloverAndLpPeripheryTempOverrides: {
+    value?: BigNumber;
+    gasLimit?: BigNumber;
+  } = {};
+
+  let marginDelta = margin;
+  if (isEth && maturedPositionSettlementBalance < margin) {
+    marginDelta = maturedPositionSettlementBalance;
+    rolloverAndLpPeripheryTempOverrides.value = BigNumber.from(margin - maturedPositionSettlementBalance);
+  }
+
+  const rolloverAndLpPeripheryParams: RolloverAndLpPeripheryParams = getRolloverAndLpPeripheryParams(
+    {
+      addLiquidity,
+      margin: marginDelta,
+      notional,
+      fixedLow,
+      fixedHigh,
+      marginEngineAddress,
+      underlyingTokenDecimals,
+      tickSpacing,
+      maturedMarginEngineAddress,
+      maturedPositionOwnerAddress,
+      maturedPositionSettlementBalance,
+      maturedPositionTickLower,
+      maturedPositionTickUpper
+    }
+  );
+
+  const estimatedGasUnits: BigNumber = await estimateRolloverAndLpGasUnits(
+    peripheryContract,
+    rolloverAndLpPeripheryParams,
+    rolloverAndLpPeripheryTempOverrides,
+  );
+
+  rolloverAndLpPeripheryTempOverrides.gasLimit = getGasBuffer(estimatedGasUnits);
+
+  const rolloverAndLpTransaction: ContractTransaction = await peripheryContract.connect(signer)
+    .rolloverAndLp(rolloverAndLpPeripheryParams, rolloverAndLpPeripheryTempOverrides)
+    .catch(() => {
+        throw new Error("RolloverAndLp Transaction Confirmation Error");
+      }
+    );
+
+  const receipt: ContractReceipt = await rolloverAndLpTransaction.wait();
+
+  return receipt;
 }
