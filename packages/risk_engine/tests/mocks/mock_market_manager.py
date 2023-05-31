f255274b5fa526c7b52882ae7eaa68b686343ac0
Artur Begyan <arturbegyan98@gmail.com>
2023-05-31 21:31:58 +0100

    feat: market mock

---
+++ packages/risk_engine/tests/mocks/mock_market_manager.py
@@ -0,0 +1,10 @@
+from unittest import mock
+from packages.risk_engine.src.instruments.dated_irs.marketManager import MarketManager
+
+
+class MockMarketManager(MarketManager):
+    def __init__(self):
+        self.get_market_by_id = mock.Mock()
+
+    def mock_get_market_by_id(self, return_value):
+        self.get_market_by_id = mock.Mock(return_value=return_value)
