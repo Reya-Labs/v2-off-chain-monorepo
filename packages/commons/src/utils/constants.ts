f52d7f33d30f0074d5bc77de6a978304830df3e2
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-06-07 14:37:46 +0100

    feat: get apy tables

--- packages/commons/src/utils/constants.ts
+++ packages/commons/src/utils/constants.ts
@@ -1,4 +1,8 @@
+import { BigNumber } from "ethers";
+
 export const SECONDS_IN_YEAR = 31_536_000;
+export const SECONDS_IN_DAY = 86_400;
+export const SECONDS_IN_HOUR = 3_600;
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
