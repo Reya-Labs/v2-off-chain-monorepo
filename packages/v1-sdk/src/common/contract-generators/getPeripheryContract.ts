dc6c587c469cb317d0a06191b6d1777fd8b6b253
Artur Begyan <arturbegyan98@gmail.com>
2023-06-07 16:50:41 +0100

    fix: bug fix

--- packages/v1-sdk/src/common/contract-generators/getPeripheryContract.ts
+++ packages/v1-sdk/src/common/contract-generators/getPeripheryContract.ts
@@ -4,7 +4,6 @@ export const getPeripheryContract = (
   peripheryAddress: string,
   provider: ethers.providers.Provider,
 ): ethers.Contract => {
-  // todo: needs to be tested separately
   const abi: string[] = [
     `
     function swap(address, bool, uint256, uint160, int24, int24, int256) external payable returns (int256,int256,uint256,int256,int256,int24,int256)
