c7846f790fb555267d1ec7fcbf83d89c575e0237
Artur Begyan <arturbegyan98@gmail.com>
2023-06-07 19:22:33 +0100

    feat: high-level setup of get info post swap

--- packages/v1-sdk/src/actions/swap/getInfoPostSwap.ts
+++ packages/v1-sdk/src/actions/swap/getInfoPostSwap.ts
@@ -1,9 +1,13 @@
 import { BigNumber, Contract } from "ethers";
+import { SwapPeripheryParams } from "../types/actionArgTypes";
+import { exponentialBackoff } from "../../common/retry";
+import { decodeInfoPostSwap } from "../../common/errors/errorHandling";
 
 
 export type GetInfoPostSwapArgs = {
   peripheryContract: Contract;
   marginEngineAddress: string;
+  swapPeripheryParams: SwapPeripheryParams;
 }
 
 export type InfoPostSwap = {
@@ -25,7 +29,8 @@ export type InfoPostSwap = {
 
 export const getInfoPostSwap = async ({
   peripheryContract,
-  marginEngineAddress
+  marginEngineAddress,
+  swapPeripheryParams
 }
 : GetInfoPostSwapArgs): Promise<InfoPostSwap> => {
 
@@ -39,8 +44,64 @@ export const getInfoPostSwap = async ({
   let fixedTokenDeltaUnbalanced = BigNumber.from(0);
   let fixedTokenDelta = BigNumber.from(0);
 
+  await peripheryContract.callStatic.swap(swapPeripheryParams).then(
+    (result: any) => {
+      availableNotional = result[1];
+      fee = result[2];
+      fixedTokenDeltaUnbalanced = result[3];
+      marginRequirement = result[4];
+      tickAfter = parseInt(result[5], 10);
+      fixedTokenDelta = result[0];
+    },
+    (error: any) => {
+      const result = decodeInfoPostSwap(error);
+      marginRequirement = result.marginRequirement;
+      tickAfter = result.tick;
+      fee = result.fee;
+      availableNotional = result.availableNotional;
+      fixedTokenDeltaUnbalanced = result.fixedTokenDeltaUnbalanced;
+      fixedTokenDelta = result.fixedTokenDelta;
+    },
+  );
+
+  const fixedRateBefore = tickToFixedRate(tickBefore);
+  const fixedRateAfter = tickToFixedRate(tickAfter);
+
+  const fixedRateDelta = fixedRateAfter.subtract(fixedRateBefore);
+  const fixedRateDeltaRaw = fixedRateDelta.toNumber();
+
+  const scaledAvailableNotional = descale(availableNotional);
+  const scaledFee = descale(fee);
+  const scaledMarginRequirement = (descale(marginRequirement) + scaledFee) * 1.01;
+
+  const additionalMargin =
+    scaledMarginRequirement > scaledCurrentMargin
+      ? scaledMarginRequirement - scaledCurrentMargin
+      : 0;
+
+  const averageFixedRate = availableNotional.eq(BigNumber.from(0))
+    ? 0
+    : fixedTokenDeltaUnbalanced.mul(BigNumber.from(1000)).div(availableNotional).toNumber() /
+    1000;
+
+  let swapGasUnits = 0;
+  const chainId = await wallet.getChainId();
+  if (Object.values(SupportedChainId).includes(chainId)) {
+    swapGasUnits = estimateSwapGasUnits(chainId);
+  }
+
+  const gasFeeNativeToken = await convertGasUnitsToNativeToken(this.provider, swapGasUnits);
+
+  const maxMarginWithdrawable = Math.max(
+    0,
+    descale(currentMargin.sub(marginRequirement).sub(BigNumber.from(1))),
+  );
 
 
+  const result: InfoPostSwap = {
 
+  }
+
+  return result;
 
 }
