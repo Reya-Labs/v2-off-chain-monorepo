77dfb6aa56555b1bab60ef19ef8bfc0923f9df12
Artur Begyan <arturbegyan98@gmail.com>
2023-06-08 13:55:33 +0100

    feat: expose init

--- packages/v1-sdk/src/index.ts
+++ packages/v1-sdk/src/index.ts
@@ -2,3 +2,4 @@ export * from './actions/settle/settle';
 export * from './actions/settle/simulateSettle';
 export * from './actions/settle/types';
 export * from './actions/swap/swap';
+export * from './init';
