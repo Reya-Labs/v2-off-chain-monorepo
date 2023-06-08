864dcdb790da2b477bebe3e56b6d8245f615efae
Artur Begyan <arturbegyan98@gmail.com>
2023-06-08 11:43:17 +0100

    feat: callstatic in settle flow

--- packages/v1-sdk/src/actions/settle/settle.ts
+++ packages/v1-sdk/src/actions/settle/settle.ts
@@ -14,6 +14,7 @@ import { PERIPHERY_ADDRESS_BY_CHAIN_ID } from '../../common/constants';
 import { PositionInfo } from '../../common/api/position/types';
 import { getPositionInfo } from '../../common/api/position/getPositionInfo';
 import { getSentryTracker } from '../../init';
+import { getReadableErrorMessage } from "../../common/errors/errorHandling";
 
 export const settle = async ({
   positionId,
@@ -46,6 +47,19 @@ export const settle = async ({
     tickLower: positionInfo.positionTickLower,
     tickUpper: positionInfo.positionTickUpper,
   };
+
+  await peripheryContract.callStatic
+    .settlePositionAndWithdrawMargin(
+      settlePeripheryParams.marginEngineAddress,
+      settlePeripheryParams.positionOwnerAddress,
+      settlePeripheryParams.tickLower,
+      settlePeripheryParams.tickUpper
+    )
+    .catch((error) => {
+      const errorMessage = getReadableErrorMessage(error);
+      console.log(errorMessage);
+      // throw new Error(errorMessage);
+    });
 
   const settlePeripheryTempOverrides: {
     value?: BigNumber;
