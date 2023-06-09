36b6a70bd72219012a47fa491ac2109f07f98ea7
Artur Begyan <arturbegyan98@gmail.com>
2023-06-09 11:40:24 +0100

    feat: fix estimate lp gas units

--- packages/v1-sdk/src/actions/lp/estimateLpGasUnits.ts
+++ packages/v1-sdk/src/actions/lp/estimateLpGasUnits.ts
@@ -1,5 +1,6 @@
 import { ethers, BigNumber } from 'ethers';
 import { LpPeripheryParams } from '../types/actionArgTypes';
+import { getReadableErrorMessage } from '../../common/errors/errorHandling';
 
 export const estimateLpGasUnits = async (
   peripheryContract: ethers.Contract,
@@ -7,9 +8,18 @@ export const estimateLpGasUnits = async (
   lpPeripheryTempOverrides: { value?: BigNumber; gasLimit?: BigNumber },
 ): Promise<BigNumber> => {
   const estimatedGas: BigNumber = await peripheryContract.estimateGas
-    .mintOrBurn(lpPeripheryParams, lpPeripheryTempOverrides)
-    .catch((error) => {
-      throw new Error('Error estimating gas cost to execute LP action.');
+    .mintOrBurn(
+      lpPeripheryParams.marginEngineAddress,
+      lpPeripheryParams.tickLower,
+      lpPeripheryParams.tickUpper,
+      lpPeripheryParams.notional,
+      lpPeripheryParams.isMint,
+      lpPeripheryParams.marginDelta,
+      lpPeripheryTempOverrides,
+    )
+    .catch(error => {
+      const errorMessage = getReadableErrorMessage(error);
+      throw new Error(errorMessage);
     });
 
   return estimatedGas;
