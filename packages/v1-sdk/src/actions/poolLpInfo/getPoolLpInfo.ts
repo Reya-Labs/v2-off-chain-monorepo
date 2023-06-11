07b23887f7ce6c81d52266606296dbd1412d8086
Artur Begyan <arturbegyan98@gmail.com>
2023-06-11 23:24:47 +0100

    feat: prep get pool lp info

---
+++ packages/v1-sdk/src/actions/poolLpInfo/getPoolLpInfo.ts
@@ -0,0 +1,12 @@
+import { GetPoolLpInfoArgs } from '../types';
+
+export type GetPoolLpInfoResults = {
+  maxLeverage: number;
+};
+
+export const getPoolLpInfo = async ({
+  ammId,
+  fixedHigh,
+  fixedLow,
+  provider,
+}: GetPoolLpInfoArgs): Promise<GetPoolLpInfoResults> => {};
