08d37e9744ff0eadcd61cfeee6e923bc0a793c9a
Artur Begyan <arturbegyan98@gmail.com>
2023-06-11 19:07:31 +0100

    feat: get balance refactor

--- packages/v1-sdk/src/actions/token/getBalance.ts
+++ packages/v1-sdk/src/actions/token/getBalance.ts
@@ -1,24 +1,31 @@
 import { GetBalanceArgs } from '../types/actionArgTypes';
-import { getERC20TokenContract } from '../../common/contract-generators';
 import { getEthBalance } from './getEthBalance';
 import { getERC20Balance } from './getERC20Balance';
+import { AMMInfo } from '../../common/api/amm/types';
+import { getAmmInfo } from '../../common/api/amm/getAmmInfo';
 
 export const getBalance = async ({
-  isEth,
-  tokenAddress,
-  tokenDecimals,
-  walletAddress,
-  provider,
+  ammId,
+  signer,
 }: GetBalanceArgs): Promise<number> => {
+  if (signer.provider === undefined) {
+    throw new Error('Signer provider not found');
+  }
+
+  const walletAddress = await signer.getAddress();
+  const provider = signer.provider;
+  const chainId: number = await signer.getChainId();
+  const ammInfo: AMMInfo = await getAmmInfo(ammId, chainId);
+
   let currentBalance: number;
-  if (isEth) {
+  if (ammInfo.isEth) {
     currentBalance = await getEthBalance({ walletAddress, provider });
   } else {
     currentBalance = await getERC20Balance({
       walletAddress,
       provider,
-      tokenAddress,
-      tokenDecimals,
+      tokenAddress: ammInfo.underlyingTokenAddress,
+      tokenDecimals: ammInfo.underlyingTokenDecimals,
     });
   }
 
