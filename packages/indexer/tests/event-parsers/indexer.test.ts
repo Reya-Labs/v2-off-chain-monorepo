1d3abd671dafc3cf7cf12e2793f6f4f08dbd4733
Ioana Roceanu <ioanaroceanu@gmail.com>
2023-06-12 16:26:36 +0100

    fix: import commons as js

---
+++ packages/indexer/tests/event-parsers/indexer.test.ts
@@ -0,0 +1,203 @@
+import { BigNumber, EventFilter } from 'ethers';
+import { sync } from '../../src/process/sync';
+import { collateralUpdateEvmEvent } from '../utils/evmEventMocks';
+import { parseCollateralUpdate } from '../../src/event-parsers/core/collateralUpdate';
+import { getCoreContract } from '../../src/contract-generators/core';
+import { getDatedIrsInstrumentContract } from '../../src/contract-generators/dated-irs-instrument';
+import { getDatedIrsVammContract } from '../../src/contract-generators/dated-irs-vamm';
+import {
+  pullCollateralUpdateEvent,
+  getBigQuery,
+  getTokenDetails,
+  convertLowercaseString,
+  insertCollateralUpdateEvent,
+} from '@voltz-protocol/commons-v2';
+
+jest.setTimeout(100_000);
+
+// Mock provider.getBlockNumber to 0
+jest.mock('../../src/services/provider.ts', () => ({
+  getProvider: () => ({
+    getBlockNumber: async () => 0,
+  }),
+}));
+
+// Mock all dependencies with blank functions
+jest.mock('../../src/contract-generators/core.ts', () => ({
+  getCoreContract: jest.fn(() => {}),
+}));
+
+jest.mock('../../src/contract-generators/dated-irs-instrument.ts', () => ({
+  getDatedIrsInstrumentContract: jest.fn(() => {}),
+}));
+
+jest.mock('../../src/contract-generators/dated-irs-vamm.ts', () => ({
+  getDatedIrsVammContract: jest.fn(() => {}),
+}));
+
+jest.mock('@voltz-protocol/commons-v2', () => ({
+  pullCollateralUpdateEvent: jest.fn(() => {}),
+  getBigQuery: jest.fn(() => {}),
+  getTokenDetails: jest.fn(() => {}),
+  convertLowercaseString: jest.fn(() => {}),
+  insertCollateralUpdateEvent: jest.fn(() => {}),
+}));
+
+// Unit tests
+describe('Collateral Update Indexer unit tests', () => {
+  it('new event', async () => {
+    // Mock core contract event filters
+    {
+      const queryFilter = jest.fn(async () => {});
+      (queryFilter as jest.Mock).mockResolvedValueOnce([
+        collateralUpdateEvmEvent,
+      ]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+
+      (getCoreContract as jest.Mock).mockReturnValueOnce({
+        queryFilter,
+        filters: {
+          CollateralUpdate: () => null as unknown as EventFilter,
+          MarketFeeConfigured: () => null as unknown as EventFilter,
+        },
+      });
+    }
+
+    // Mock instrument contract event filters
+    {
+      const queryFilter = jest.fn(async () => {});
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+
+      (getDatedIrsInstrumentContract as jest.Mock).mockReturnValueOnce({
+        queryFilter,
+        filters: {
+          MarketConfigured: () => null as unknown as EventFilter,
+          RateOracleConfigured: () => null as unknown as EventFilter,
+          ProductPositionUpdated: () => null as unknown as EventFilter,
+        },
+      });
+    }
+
+    // Mock vamm contract event filters
+    {
+      const queryFilter = jest.fn(async () => {});
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+
+      (getDatedIrsVammContract as jest.Mock).mockReturnValueOnce({
+        queryFilter,
+        filters: {
+          VammCreated: () => null as unknown as EventFilter,
+          VammPriceChange: () => null as unknown as EventFilter,
+        },
+      });
+    }
+
+    // Mock getBigQuery
+    const query = jest.fn(async () => {});
+
+    (getBigQuery as jest.Mock).mockReturnValueOnce({
+      query,
+    });
+
+    (getTokenDetails as jest.Mock).mockReturnValue({
+      tokenDescaler: (value: BigNumber) => 100,
+    });
+
+    // Mock pullCollateralUpdateEvent
+    (pullCollateralUpdateEvent as jest.Mock).mockResolvedValueOnce(null);
+
+    // Mock insertCollateralUpdateEvent
+    (insertCollateralUpdateEvent as jest.Mock).mockResolvedValueOnce(query());
+
+    // Fire call
+    await sync([1]);
+
+    // Check expectations
+    expect(query.mock.calls.length).toBe(1);
+  });
+
+  it('existing event', async () => {
+    // Mock core contract event filters
+    {
+      const queryFilter = jest.fn(async () => {});
+      (queryFilter as jest.Mock).mockResolvedValueOnce([
+        collateralUpdateEvmEvent,
+      ]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+
+      (getCoreContract as jest.Mock).mockReturnValueOnce({
+        queryFilter,
+        filters: {
+          MarketFeeConfigured: () => null as unknown as EventFilter,
+          CollateralUpdate: () => null as unknown as EventFilter,
+        },
+      });
+    }
+
+    // Mock instrument contract event filters
+    {
+      const queryFilter = jest.fn(async () => {});
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+
+      (getDatedIrsInstrumentContract as jest.Mock).mockReturnValueOnce({
+        queryFilter,
+        filters: {
+          MarketConfigured: () => null as unknown as EventFilter,
+          RateOracleConfigured: () => null as unknown as EventFilter,
+          ProductPositionUpdated: () => null as unknown as EventFilter,
+        },
+      });
+    }
+
+    // Mock vamm contract event filters
+    {
+      const queryFilter = jest.fn(async () => {});
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+      (queryFilter as jest.Mock).mockResolvedValueOnce([]);
+
+      (getDatedIrsVammContract as jest.Mock).mockReturnValueOnce({
+        queryFilter,
+        filters: {
+          VammCreated: () => null as unknown as EventFilter,
+          VammPriceChange: () => null as unknown as EventFilter,
+        },
+      });
+    }
+
+    // Mock getBigQuery
+    const query = jest.fn(async () => {});
+
+    (getBigQuery as jest.Mock).mockReturnValueOnce({
+      query,
+    });
+
+    (getTokenDetails as jest.Mock).mockReturnValue({
+      tokenDescaler: (value: BigNumber) => 0,
+    });
+
+    (convertLowercaseString as jest.Mock).mockReturnValueOnce('');
+
+    (insertCollateralUpdateEvent as jest.Mock).mockReturnValueOnce(null);
+
+    // Mock pullCollateralUpdateEvent
+    const collateralUpdateEvent = parseCollateralUpdate(
+      1,
+      collateralUpdateEvmEvent,
+    );
+
+    (pullCollateralUpdateEvent as jest.Mock).mockResolvedValueOnce(
+      collateralUpdateEvent,
+    );
+
+    // Fire call
+    await sync([1]);
+
+    // Check expectations
+    expect(query.mock.calls.length).toBe(0);
+  });
+});
