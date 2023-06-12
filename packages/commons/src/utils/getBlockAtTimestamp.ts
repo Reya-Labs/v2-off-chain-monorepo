385c5381d244d6440b5b55ee44fee4bbb989eee4
tudor47 <tudorcostin7@gmail.com>
2023-06-12 18:20:58 +0300

    fix: lint

--- packages/commons/src/utils/getBlockAtTimestamp.ts
+++ packages/commons/src/utils/getBlockAtTimestamp.ts
@@ -1,26 +1,5 @@
 import { ethers } from 'ethers';
 
-import { SECONDS_IN_YEAR } from './constants';
-
-export const getTimestampInSeconds = (): number => {
-  return Math.floor(Date.now() / 1000);
-};
-
-export const getTimeInYearsBetweenTimestamps = (
-  from: number,
-  to: number,
-): number => {
-  if (from > to) {
-    throw new Error(`Unordered timestamps ${from}-${to}`);
-  }
-
-  return (to - from) / SECONDS_IN_YEAR;
-};
-
-export const sleep = (ms: number): Promise<void> => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
 export async function getBlockAtTimestamp(
   provider: ethers.providers.Provider,
   timestamp: number,
