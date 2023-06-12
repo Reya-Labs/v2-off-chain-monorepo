e1f8a8cd6deb8e66f8f865b5c38adcd12d763abd
Artur Begyan <arturbegyan98@gmail.com>
2023-06-12 23:40:15 +0100

    feat: estimate gas native token

--- packages/v1-sdk/src/actions/lp/getInfoPostLp.ts
+++ packages/v1-sdk/src/actions/lp/getInfoPostLp.ts
@@ -1,6 +1,7 @@
 import { BigNumber, Contract, providers, Signer } from 'ethers';
-import { SupportedChainId } from '../../common/types';
 import { LpPeripheryParams } from '../types';
+import { estimateLpGasUnits } from './estimateLpGasUnits';
+import { convertGasUnitsToNativeTokenUnits } from '../../common';
 
 export type InfoPostLp = {
   marginRequirement: number;
@@ -30,6 +31,7 @@ export const getInfoPostLp = async ({
   const marginRequirement: number = await getMarginRequirementPostLp({
     peripheryContract,
     lpPeripheryParams,
+    underlyingTokenDecimals,
   });
 
   const maxMarginWithdrawable: number = await getMaxMarginWithdrawablePostLp({
@@ -38,5 +40,16 @@ export const getInfoPostLp = async ({
     marginRequirement,
     tickLower: lpPeripheryParams.tickLower,
     tickUpper: lpPeripheryParams.tickUpper,
+    underlyingTokenDecimals,
   });
+
+  // todo: test to number conversion does not overflow
+  const lpGasUnits = (
+    await estimateLpGasUnits(peripheryContract, lpPeripheryParams)
+  ).toNumber();
+
+  const gasFeeNativeToken = await convertGasUnitsToNativeTokenUnits(
+    peripheryContract.provider,
+    lpGasUnits,
+  );
 };
