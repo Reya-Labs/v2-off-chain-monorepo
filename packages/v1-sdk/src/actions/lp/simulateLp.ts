b7a5ae206c2d32e3cb9b2810890a022761148502
Artur Begyan <arturbegyan98@gmail.com>
2023-06-13 01:02:53 +0100

    feat: simulate lp end to end

--- packages/v1-sdk/src/actions/lp/simulateLp.ts
+++ packages/v1-sdk/src/actions/lp/simulateLp.ts
@@ -1,10 +1,58 @@
-import { LpArgs } from '../types';
+import { LpArgs, LpPeripheryParams } from '../types';
+import { getInfoPostLp, InfoPostLp } from './getInfoPostLp';
+import { AMMInfo } from '../../common/api/amm/types';
+import { getAmmInfo } from '../../common/api/amm/getAmmInfo';
+import {
+  DEFAULT_TICK_SPACING,
+  PERIPHERY_ADDRESS_BY_CHAIN_ID,
+} from '../../common/constants';
+import { ethers } from 'ethers';
+import { getPeripheryContract } from '../../common/contract-generators';
+import { getMarginEngineContract } from '../../common/contract-generators';
+import { getLpPeripheryParams } from './getLpPeripheryParams';
 
-export const simulateLp = ({
+export const simulateLp = async ({
   ammId,
   fixedLow,
   fixedHigh,
   notional,
   margin,
   signer,
-}: LpArgs) => {};
+}: LpArgs): Promise<InfoPostLp> => {
+  const chainId: number = await signer.getChainId();
+  const ammInfo: AMMInfo = await getAmmInfo(ammId, chainId);
+  const tickSpacing: number = DEFAULT_TICK_SPACING;
+  const walletAddress: string = await signer.getAddress();
+
+  const peripheryAddress = PERIPHERY_ADDRESS_BY_CHAIN_ID[chainId];
+
+  const peripheryContract: ethers.Contract = getPeripheryContract(
+    peripheryAddress,
+    signer,
+  );
+
+  const marginEngineContract: ethers.Contract = getMarginEngineContract(
+    ammInfo.marginEngineAddress,
+    signer,
+  );
+
+  const lpPeripheryParams: LpPeripheryParams = getLpPeripheryParams({
+    margin,
+    notional,
+    fixedLow,
+    fixedHigh,
+    marginEngineAddress: ammInfo.marginEngineAddress,
+    underlyingTokenDecimals: ammInfo.underlyingTokenDecimals,
+    tickSpacing,
+  });
+
+  const infoPostLp = await getInfoPostLp({
+    peripheryContract,
+    marginEngineContract,
+    walletAddress,
+    underlyingTokenDecimals: ammInfo.underlyingTokenDecimals,
+    lpPeripheryParams,
+  });
+
+  return infoPostLp;
+};
