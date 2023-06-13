c776e01bff34eba5b35098c6c7c7c8d433a73aa7
Artur Begyan <arturbegyan98@gmail.com>
2023-06-13 23:19:09 +0100

    feat: simplified simulate rollover with swap

--- packages/v1-sdk/src/actions/rolloverWithLp/simulateRolloverWithLp.ts
+++ packages/v1-sdk/src/actions/rolloverWithLp/simulateRolloverWithLp.ts
@@ -1,1 +1,23 @@
-export const simulateRolloverWithLp = async () => {};
+import { RolloverAndLpArgs } from '../types';
+import { InfoPostLp } from '../lp/getInfoPostLp';
+import { simulateLp } from '../lp';
+
+export const simulateRolloverWithLp = async ({
+  maturedPositionId,
+  ammId,
+  fixedLow,
+  fixedHigh,
+  notional,
+  margin,
+  signer,
+}: RolloverAndLpArgs): Promise<InfoPostLp> => {
+  // todo: currently uses infopostlp instead of infopostrolloverwithlp
+  return simulateLp({
+    ammId,
+    fixedLow,
+    fixedHigh,
+    notional,
+    margin,
+    signer,
+  });
+};
