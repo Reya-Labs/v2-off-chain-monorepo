30d4a35dedc2e34ae475d36222fb5374ab30503b
Artur Begyan <arturbegyan98@gmail.com>
2023-06-13 23:22:20 +0100

    feat: simulate rollover with swap

--- packages/v1-sdk/src/actions/rolloverWithSwap/simulateRolloverWithSwap.ts
+++ packages/v1-sdk/src/actions/rolloverWithSwap/simulateRolloverWithSwap.ts
@@ -1,1 +1,19 @@
-export const simulateRolloverWithSwap = async () => {};
+import { simulateSwap } from '../swap';
+import { RolloverAndSwapArgs } from '../types';
+import { InfoPostSwap } from '../swap/getInfoPostSwap';
+
+export const simulateRolloverWithSwap = async ({
+  maturedPositionId,
+  ammId,
+  notional,
+  margin,
+  signer,
+}: RolloverAndSwapArgs): Promise<InfoPostSwap> => {
+  // todo: replcae with infopostrolloverwithswap based impl
+  return simulateSwap({
+    ammId,
+    notional,
+    margin,
+    signer,
+  });
+};
