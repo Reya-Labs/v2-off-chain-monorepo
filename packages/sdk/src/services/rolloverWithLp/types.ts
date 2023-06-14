840fde13b1624decee194b03d38128b60f44b6d3
Artur Begyan <arturbegyan98@gmail.com>
2023-06-14 14:30:41 +0100

    feat: update args

--- packages/sdk/src/services/rolloverWithLp/types.ts
+++ packages/sdk/src/services/rolloverWithLp/types.ts
@@ -1,22 +1,11 @@
-import { providers, Signer } from 'ethers';
+import { Signer } from 'ethers';
 
 export type RolloverAndLpArgs = {
+  maturedPositionId: string;
+  ammId: string;
   fixedLow: number;
   fixedHigh: number;
   notional: number;
   margin: number;
-  underlyingTokenAddress: string;
-  underlyingTokenDecimals: number;
-  tickSpacing: number;
-  chainId: number;
-  peripheryAddress: string;
-  marginEngineAddress: string;
-  provider: providers.Provider;
   signer: Signer;
-  isEth: boolean;
-  maturedMarginEngineAddress: string;
-  maturedPositionOwnerAddress: string;
-  maturedPositionSettlementBalance: number;
-  maturedPositionTickLower: number;
-  maturedPositionTickUpper: number;
 };
