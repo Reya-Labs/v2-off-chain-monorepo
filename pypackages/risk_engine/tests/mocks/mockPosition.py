9d657739eeb3c96eed3674f04730d8085f163486
Artur Begyan <arturbegyan98@gmail.com>
2023-06-15 00:45:37 +0100

    feat: linting

--- pypackages/risk_engine/tests/mocks/mockPosition.py
+++ pypackages/risk_engine/tests/mocks/mockPosition.py
@@ -4,20 +4,22 @@ mock_position = {
         "pool_id": "pool_irs_usdc",
         "base_token": "USDC",
         "maturity": 30,
-        "simulation_set": tuple([
-            "apy_OracleSimulator",
-            "apy_scaled_0.05_OracleSimulator",
-            "apy_scaled_0.1_OracleSimulator",
-            "apy_scaled_0.15_OracleSimulator",
-            "apy_scaled_0.2_OracleSimulator",
-            "apy_scaled_0.5_OracleSimulator",
-            #"apy_scaled_0.75_OracleSimulator",
-            #"apy_scaled_1_OracleSimulator",
-            #"apy_shock_OracleSimulator",
-        ]),
+        "simulation_set": tuple(
+            [
+                "apy_OracleSimulator",
+                "apy_scaled_0.05_OracleSimulator",
+                "apy_scaled_0.1_OracleSimulator",
+                "apy_scaled_0.15_OracleSimulator",
+                "apy_scaled_0.2_OracleSimulator",
+                "apy_scaled_0.5_OracleSimulator",
+                # "apy_scaled_0.75_OracleSimulator",
+                # "apy_scaled_1_OracleSimulator",
+                # "apy_shock_OracleSimulator",
+            ]
+        ),
         "maker_amount": 10000,
         "taker_amount": 1000,
         "phi": 2e-5,
-        "beta": 1.02
+        "beta": 1.02,
     }
 }
