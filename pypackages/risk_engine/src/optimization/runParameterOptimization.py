c86f92787be33c82a6aad99ca0ea870f87e69cd2
Artur Begyan <arturbegyan98@gmail.com>
2023-06-15 23:22:16 +0100

    feat: run parameter optimization simplified

--- pypackages/risk_engine/src/optimization/runParameterOptimization.py
+++ pypackages/risk_engine/src/optimization/runParameterOptimization.py
@@ -1,39 +1,39 @@
 import json
 import optuna
-
+import os
+from objectiveFunction import objective
+from optuna import Study
+from optuna.trial import FrozenTrial
 
 def run_param_optimization(parser):
 
     parser.add_argument(
         "-n_trials", "--n_trials", type=float, help="Number of optimization trials", default=2
     )
+    parser.add_argument(
+        "-market_name", "--market_name", type=str, help="Name of market", default="irs_usdc"
+    )
     parser.add_argument("-d", "--debug", action="store_true", help="Debug mode", default=False)
     n_trials = parser.parse_args().n_trials
+    market_name = parser.parse_args().market_name
 
-    study = optuna.create_study(
+    study: Study = optuna.create_study(
         direction="maximize",
         sampler=optuna.samplers.TPESampler(),
         pruner=optuna.pruners.SuccessiveHalvingPruner(),
     )
     study.optimize(objective, n_trials=n_trials)
 
-    # Relevant output plots
-    out_dir = f"./{MARKET}/optuna_final/"
+    out_dir = f"./{market_name}/optuna_final/"
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    # Output optimised results
-    trial = study.best_trial
-    print(f"Best optimised value: {trial.value}")
-
-    print("Optimised parameters: ")
-    for key, value in trial.params.items():
-        print(f"{key}: {value}")
+    trial: FrozenTrial = study.best_trial
 
     fig = optuna.visualization.plot_optimization_history(study)
-    fig.write_image(out_dir + f"optuna_history_{MARKET}.png")
+    fig.write_image(out_dir + f"optuna_history_{market_name}.png")
 
-    with open(out_dir + f"optimised_parameters_{MARKET}.json", "w") as fp:
+    with open(out_dir + f"optimised_parameters_{market_name}.json", "w") as fp:
         json.dump(trial.params, fp, indent=4)
 
 
