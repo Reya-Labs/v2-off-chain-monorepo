8707970cefb4f1cf0263ab2530132aa9b900c81e
Artur Begyan <arturbegyan98@gmail.com>
2023-06-15 11:23:51 +0100

    feat: constants for margin requirement simulation

--- pypackages/risk_engine/tests/end_to_end/simulations/margin_requirements_test.py
+++ pypackages/risk_engine/tests/end_to_end/simulations/margin_requirements_test.py
@@ -6,8 +6,23 @@ class MarginRequirementsTest(unittest.TestCase):
 
 
     def setUp(self):
+        # collateral_token,
+        # initial_fixed_rate,
+        # risk_parameter,
+        # im_multiplier,
+        # slippage_phi,
+        # slippage_beta,
+        # lp_spread,
+        # is_trader_vt,
+        # timestamps,
+        # indices,
+        # maker_fee,
+        # taker_fee,
+        # gwap_lookback
 
-        pass
+        self.margin_requirements_sim = MarginRequirements(
+
+        )
 
     def full_run_test(self):
 
