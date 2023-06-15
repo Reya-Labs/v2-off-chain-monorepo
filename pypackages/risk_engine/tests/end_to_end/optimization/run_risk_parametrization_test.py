5e18d96b40c0e79ea01dfbcda7d67177c7467eb2
Artur Begyan <arturbegyan98@gmail.com>
2023-06-15 22:31:18 +0100

    feat: add test files for key functions in optimization flows

--- pypackages/risk_engine/tests/end_to_end/optimization/run_risk_parametrization_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from risk_engine.src.optimization.runRiskOptimization import run_param_optimization
-
-class RunRiskOptimizationTest(unittest.TestCase):
-
-    def setUp(self):
-
-        pass
-
