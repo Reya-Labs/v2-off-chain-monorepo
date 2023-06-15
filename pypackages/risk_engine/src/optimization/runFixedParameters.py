7965fc74416dee6cea3ea1413dd87ebce23dea2d
Artur Begyan <arturbegyan98@gmail.com>
2023-06-15 22:28:38 +0100

    feat: run with a single set of params

--- pypackages/risk_engine/src/optimization/runFixedParameters.py
+++ pypackages/risk_engine/src/optimization/runFixedParameters.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from risk_engine.src.optimization.generatePool import generate_pool
+import numpy as np
 
-
-def run_with_a_single_set_of_params(parser, positions):
+# todo: layer in typings (esp for positions)
+def run_with_a_single_set_of_params(parser, positions, oracle_data_dir):
 
     parser.add_argument("-plm", "--p_lm", type=float, help="p_lm risk matrix factor", default=2.0)
     parser.add_argument("-gam", "--gamma", type=float, help="gamma factor for LP->IM conversion", default=1.5)
@@ -11,31 +12,28 @@ def run_with_a_single_set_of_params(parser, positions):
     parser.add_argument("-s", "--spread", type=float, help="LP spread", default=0.01)
     parser.add_argument("-l", "--lookback", type=int, help="GWAP lookback window", default=2)
 
-    tuneables = parser.parse_args()
-
-    # Defining dictionary for the tuneable parameters
-    tuneable_dict = dict((k, v) for k, v in vars(tuneables).items() if v is not None)
-    print(tuneable_dict)
+    parameters = parser.parse_args()
+    parameters_dict = dict((k, v) for k, v in vars(parameters).items() if v is not None)
 
     # todo: simplify and expose in another function (duiplicate)
-    sim_dir = "../MarketTradingSimulator/simulations/csv/"
     oracles = [
-        pd.read_csv(sim_dir + s + ".csv") for s in positions["simulation_set"]
+        pd.read_csv(oracle_data_dir + s + ".csv") for s in positions["simulation_set"]
     ]  # Get the set of all simulations
     optimisations = [
         generate_pool(
             oracles[i],
             positions["simulation_set"][i],
-            p_lm,
-            gamma,
-            lambda_taker,
-            lambda_maker,
-            spread,
-            lookback,
+            p_lm=parameters_dict['p_lm'],
+            gamma=parameters_dict['gamma'],
+            lambda_taker=parameters_dict['lambda_taker'],
+            lambda_maker=parameters_dict['lambda_maker'],
+            spread=parameters_dict['lambda_maker'],
+            lookback=parameters_dict['lookback'],
             min_leverage=20,
         )
         for i in range(len(oracles))
     ]
+    # todo: check in on this, do we want to return the average of optimizations
     return np.mean(
         optimisations
-    )  # We will optimise by averaging across all of the markets. TODO: confirm this is reasonabl
+    )
