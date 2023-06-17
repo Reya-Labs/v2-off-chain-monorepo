74298b5017b8c9c8a648d41c2a7db2446f8e73bc
Artur Begyan <arturbegyan98@gmail.com>
2023-06-17 16:30:23 +0100

    feat: add typings to run optuna trial parameters

--- pypackages/risk_engine/src/optimization/runParameterOptimization.py
+++ pypackages/risk_engine/src/optimization/runParameterOptimization.py
@@ -1,7 +1,7 @@
 import json
 import optuna
 import os
-from calculateObjectiveOptunaTrial import objective
+from runOptunaTrialParameters import calculate_objective_optuna_trial
 from optuna import Study
 from optuna.trial import FrozenTrial
 
@@ -22,7 +22,7 @@ def run_param_optimization(parser):
         sampler=optuna.samplers.TPESampler(),
         pruner=optuna.pruners.SuccessiveHalvingPruner(),
     )
-    study.optimize(objective, n_trials=n_trials)
+    study.optimize(calculate_objective_optuna_trial, n_trials=n_trials)
 
     out_dir = f"./{market_name}/optuna_final/"
     if not os.path.exists(out_dir):
@@ -35,8 +35,6 @@ def run_param_optimization(parser):
 
     with open(out_dir + f"optimised_parameters_{market_name}.json", "w") as fp:
         json.dump(trial.params, fp, indent=4)
-
-
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
