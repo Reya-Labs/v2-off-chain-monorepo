0fd1f1fc1494b138d3125b7ab5a970fdb0651def
Artur Begyan <arturbegyan98@gmail.com>
2023-06-18 17:10:32 +0100

    feat: wrap slippage model test setup

--- pypackages/risk_engine/tests/unit/slippage/run_slippage_model_optimization_test.py
+++ pypackages/risk_engine/tests/unit/slippage/run_slippage_model_optimization_test.py
@@ -1,22 +1,36 @@
 import unittest
 from risk_engine.src.slippage.runSlippageModelOptimization import run_slippage_model_optimization
 from risk_engine.src.slippage.calculateSlippageModel import calculate_slippage_model
-import pandas as pd
 from pandas import Series
+from risk_engine.src.slippage.slippageModelParameters import SlippageModelParameters
 
-TRUE_BETA = 0.5
-TRUE_PHI = 0.00001
+TRUE_SLIPPAGE_BETA = 0.5
+TRUE_SLIPPAGE_PHI = 0.00001
 
 class SlippageModelOptimizationTest(unittest.TestCase):
 
     def setUp(self):
 
+        self.true_slippage_model_parameters = SlippageModelParameters(
+            slippage_phi=TRUE_SLIPPAGE_PHI,
+            slippage_beta=TRUE_SLIPPAGE_BETA
+        )
+
         self.notional: Series = Series(
             data=[10.0, 100.0, 1000.0, 10000.0]
         )
 
-        self.slippage =
+        self.slippage: Series = calculate_slippage_model(
+            slippage_model_parameters=self.true_slippage_model_parameters,
+            notional_in_quote=self.notional
+        )
 
     def test_run_slippage_model_optimization(self):
 
-        pass
+        estimated_slippage_model_parametesr = run_slippage_model_optimization(
+            slippage=self.slippage,
+            notional=self.notional
+        )
+
+        self.assertAlmostEqual(estimated_slippage_model_parametesr.slippage_beta, TRUE_SLIPPAGE_BETA)
+        self.assertAlmostEqual(estimated_slippage_model_parametesr.slippage_phi, TRUE_SLIPPAGE_PHI)
