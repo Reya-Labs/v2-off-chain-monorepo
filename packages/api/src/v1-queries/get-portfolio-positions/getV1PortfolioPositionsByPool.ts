0f99c7b3fe4b2359daad25d6a7da471e2fc784c3
Costin-Razvan Tudor <39092730+0xZenus@users.noreply.github.com>
2023-06-23 18:53:55 +0300

    feat: endpoints for pool positions (#107)

---
+++ packages/api/src/v1-queries/get-portfolio-positions/getV1PortfolioPositionsByPool.ts
@@ -0,0 +1,55 @@
+import { decodeV1PoolId, encodeV1PositionId } from '@voltz-protocol/commons-v2';
+
+import { getPositions as getRawPositions } from '@voltz-protocol/subgraph-data';
+import { getSubgraphURL } from '../../old-v1-queries/subgraph/getSubgraphURL';
+import { getV1PortfolioPositionDetails } from './getV1PortfolioPositionDetails';
+import { V1PortfolioPositionDetails } from './types';
+
+export const getV1PortfolioPositionsByPool = async (
+  poolId: string,
+  ownerAddress: string,
+  type: 'trader' | 'lp',
+): Promise<V1PortfolioPositionDetails[]> => {
+  const now = Date.now().valueOf();
+
+  const { chainId, vammAddress } = decodeV1PoolId(poolId);
+  const subgraphURL = getSubgraphURL(chainId);
+  let positions = subgraphURL
+    ? await getRawPositions(subgraphURL, now, {
+        owners: [ownerAddress],
+        ammIDs: [vammAddress],
+      })
+    : [];
+
+  if (type === 'trader') {
+    positions = positions.filter(
+      (p) => p.positionType === 1 || p.positionType === 2,
+    );
+  } else {
+    positions = positions.filter((p) => p.positionType === 3);
+  }
+
+  const responses = await Promise.allSettled(
+    positions.map((p) =>
+      getV1PortfolioPositionDetails({
+        positionId: encodeV1PositionId({
+          chainId,
+          vammAddress: p.amm.id,
+          ownerAddress: p.owner,
+          tickLower: p.tickLower,
+          tickUpper: p.tickUpper,
+        }),
+        includeHistory: true,
+      }),
+    ),
+  );
+
+  const processedPositions = responses.map((r) => {
+    if (r.status === 'rejected') {
+      throw r.reason;
+    }
+    return r.value;
+  });
+
+  return processedPositions;
+};
