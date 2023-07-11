db323554362275c34fc9a1edce5be6c5e0c1a73f
tudor47 <tudorcostin7@gmail.com>
2023-07-12 02:24:38 +0300

    feat: variable/fixed rates

--- packages/api/src/v1v2-queries/get-fixed-rates/getV1V2VariableRates.ts
+++ packages/api/src/v1v2-queries/get-fixed-rates/getV1V2VariableRates.ts
@@ -1,24 +1,50 @@
 import { getVariableRates } from '@voltz-protocol/indexer-v1';
 import { getV1V2Pool } from '../get-pools/getV1V2Pool';
 import { HistoricalRate } from '@voltz-protocol/api-v2-types';
+import { convertLowercaseString } from '@voltz-protocol/commons-v2';
+import { getVariableRatesV2 } from '../../v2-queries/get-variable-rates/getVariableRatesV2';
 
 export const getV1V2VariableRates = async (
   poolId: string,
   fromSeconds: number,
   toSeconds: number,
 ): Promise<HistoricalRate[]> => {
-  const v1v2Pool = await getV1V2Pool(poolId);
+  if (poolId.endsWith('v1')) {
+    try {
+      const v1Pool = await getV1V2Pool(poolId);
 
-  if (v1v2Pool.isV2) {
-    // todo: to be implemented
-    return [];
+      const variableRates = getVariableRates(
+        v1Pool.chainId,
+        v1Pool.rateOracle.address,
+        fromSeconds,
+        toSeconds,
+      );
+
+      return variableRates;
+    } catch (error) {
+      console.error(`Unable to fetch variable rates for V1 pool ${poolId}`);
+      return [];
+    }
   }
 
-  // v1
-  return getVariableRates(
-    v1v2Pool.chainId,
-    v1v2Pool.rateOracle.address,
-    fromSeconds,
-    toSeconds,
-  );
+  if (poolId.endsWith('v2')) {
+    try {
+      const v2Pool = await getV1V2Pool(poolId);
+
+      const variableRates = await getVariableRatesV2(
+        v2Pool.chainId,
+        convertLowercaseString(v2Pool.rateOracle.address),
+        fromSeconds,
+        toSeconds,
+      );
+
+      return variableRates;
+    } catch (error) {
+      console.error(`Unable to fetch variable rates for V2 pool ${poolId}`);
+      return [];
+    }
+  }
+
+  console.error(`Could not find V1V2 pool with id ${poolId}`);
+  return [];
 };
--- packages/v2-bigquery/src/liquidity-indices-table/index.ts
+++ packages/v2-bigquery/src/liquidity-indices-table/index.ts
@@ -2,4 +2,5 @@ export * from './operations/getLiquidityIndexAt';
 export * from './operations/insertEntry';
 export * from './operations/getApyFromTo';
 export * from './operations/pullLiquidityIndices';
+export * from './operations/pullLiquidityIndicesByRateOracle';
 export { LiquidityIndexEntry } from './specific';
