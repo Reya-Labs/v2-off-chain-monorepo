b75bb5ceb61bb07aaa9abbe10766f3398b99bb94
tudor47 <tudorcostin7@gmail.com>
2023-07-21 21:46:06 +0300

    feat: blacklist one pool

--- packages/api/src/services/isPoolBlacklisted.ts
+++ packages/api/src/services/isPoolBlacklisted.ts
@@ -1,6 +1,7 @@
 const BLACKLISTED_POOL_IDS: Record<string, boolean> = {
   '421613_1_1689940800_v2': true,
   '421613_1_1689940801_v2': true,
+  '421613_1_1690200000_v2': true,
 };
 
 export const isPoolBlacklisted = (poolId: string): boolean => {
