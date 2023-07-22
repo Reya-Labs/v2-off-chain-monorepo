3d8fc20205281b1dd7fdcece8d21bb873c4228b7
ssbarbee <ssbarbee@users.noreply.github.com>
2023-07-22 10:08:27 +0200

    feat: add storybook configs

---
+++ packages/api-sdk/.storybook/preview.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import darkTheme from './themes/dark';
+import { Page } from 'brokoli-ui';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  layout: 'fullscreen',
+  docs: {
+    theme: darkTheme,
+  },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+};
+
+addDecorator((story) => {
+  return <Page>{story()}</Page>;
+});
