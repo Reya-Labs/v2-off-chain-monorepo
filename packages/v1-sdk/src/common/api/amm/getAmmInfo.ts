c61bb5004899cb6da9790b1ba93d49af88ce3231
ssbarbee <ssbarbee@users.noreply.github.com>
2023-07-22 10:34:29 +0200

    fix: broken integrations

--- packages/v1-sdk/src/common/api/amm/getAmmInfo.ts
+++ packages/v1-sdk/src/common/api/amm/getAmmInfo.ts
@@ -2,14 +2,12 @@ import { AMMInfo } from './types';
 import { getPool } from '@voltz-protocol/api-sdk-v2';
 
 export const getAmmInfo = async (ammId: string): Promise<AMMInfo> => {
-  const rawAMM = await getPool(ammId);
+  const rawAMM = await getPool({ poolId: ammId });
 
-  const ammInfo: AMMInfo = {
+  return {
     isEth: rawAMM.underlyingToken.name === 'eth',
     marginEngineAddress: rawAMM.marginEngineAddress,
     underlyingTokenDecimals: rawAMM.underlyingToken.tokenDecimals,
     underlyingTokenAddress: rawAMM.underlyingToken.address,
   };
-
-  return ammInfo;
 };
