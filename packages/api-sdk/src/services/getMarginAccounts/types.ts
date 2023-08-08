29b1cf3b89261fc44713954087c078a5398e2541
ssbarbee <ssbarbee@users.noreply.github.com>
2023-08-08 08:48:28 +0200

    feat: add getMarginAccounts

---
+++ packages/api-sdk/src/services/getMarginAccounts/types.ts
@@ -0,0 +1,35 @@
+import { SupportedChainId } from '@voltz-protocol/commons-v2';
+
+export type MarginAccountsSortBy =
+  | 'balance'
+  | 'marginRatio'
+  | 'positionsLength';
+export type SortDirection = 'noSort' | 'ascending' | 'descending';
+
+export type GetMarginAccountsArgs = {
+  chainIds: number[];
+  ownerAddress: string;
+  sort: {
+    id: MarginAccountsSortBy;
+    direction: SortDirection;
+  };
+  page: number;
+  perPage: number;
+};
+
+export type MarginRatioHealth = 'healthy' | 'danger' | 'warning';
+
+export type MarginAccount = {
+  id: string;
+  chainId: SupportedChainId;
+  name: string;
+  balance: number;
+  positionsCount: number;
+  marginRatioPercentage: number;
+  marginRatioHealth: MarginRatioHealth;
+};
+
+export type GetMarginAccountsResponse = {
+  marginAccounts: MarginAccount[];
+  totalMarginAccounts: number;
+};
