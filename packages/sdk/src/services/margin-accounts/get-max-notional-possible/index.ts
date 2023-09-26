2429f2928de7c9c625a122ee848722b836f9c57f
ssbarbee <ssbarbee@users.noreply.github.com>
2023-09-26 10:54:05 +0200

    feat: add getMaxNotionalPossible

---
+++ packages/sdk/src/services/margin-accounts/get-max-notional-possible/index.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export type GetMaxNotionalPossibleArgs = {
+  poolId: string;
+  marginAccountId: string;
+};
+
+export const getMaxNotionalPossible = async ({
+  poolId,
+  marginAccountId,
+}: GetMaxNotionalPossibleArgs): Promise<number> => {
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const num = Math.random() * 100;
+      if (num > 50) {
+        reject(new Error('Random error happened. Chance is 50-50 try again!'));
+        return;
+      }
+
+      resolve(Math.random() * 10000 + 1000);
+    }, Math.random() * 100 + 1000);
+  });
+};
--- packages/sdk/src/services/margin-accounts/index.ts
+++ packages/sdk/src/services/margin-accounts/index.ts
@@ -3,3 +3,4 @@ export * from './withdraw-margin';
 export * from './deposit-margin';
 export * from './simulate-deposit-margin';
 export * from './create-margin-account';
+export * from './get-max-notional-possible';
