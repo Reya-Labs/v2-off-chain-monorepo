027809209921f0db5e616afdd963d441299a9545
ssbarbee <ssbarbee@users.noreply.github.com>
2023-10-03 14:34:02 +0200

    fix: remove

--- packages/api-sdk/src/services/getMarginAccounts/mocks.ts
+++ packages/api-sdk/src/services/getMarginAccounts/mocks.ts
@@ -117,19 +117,6 @@ function getRandomTwoLetterSentenceWithEmoji() {
   return `${randomSentence} ${randomEmoji}`;
 }
 
-function generateRandomId(length = 8) {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let randomId = '';
-
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    randomId += characters.charAt(randomIndex);
-  }
-
-  return randomId;
-}
-
 const randomMarginAccount = (
   index: number,
 ): GetMarginAccountsResponse['marginAccounts'][0] => ({
